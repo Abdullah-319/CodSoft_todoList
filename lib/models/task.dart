08f4aaeafc872572497f666e48f8dd0730b844d9
Abdullah Ahmad <abdullahahmad319.319@gmail.com>
2023-12-08 00:44:42 +0500

    todo screen done

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,6 +1,7 @@
 class Task {
-  Task({required this.title, required this.description});
+  Task({required this.title, required this.date, required this.description});
 
   final String title;
+  final DateTime date;
   final String description;
 }
--- lib/screens/todo.dart
+++ lib/screens/todo.dart
@@ -9,6 +9,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final todoTasks = [];
+
+  void _moveToDoneScreen() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,24 +20,106 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Text(
+                'Daily Tasks',
+                style: GoogleFonts.inter(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            GestureDetector(
+              onTap: _moveToDoneScreen,
+              child: Container(
+                height: 69,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(22),
+                  color: const Color.fromARGB(255, 23, 224, 188),
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 12),
+                    Image.asset(
+                      'lib/assets/done.png',
+                      height: 30,
+                      width: 30,
+                      fit: BoxFit.cover,
+                    ),
+                    Text(
+                      'Completed',
+                      style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 45),
             Text(
-              'Daily Tasks',
+              'To do Tasks',
               style: GoogleFonts.inter(
                 color: Colors.white,
-                fontSize: 30,
+                fontSize: 20,
                 fontWeight: FontWeight.w700,
               ),
             ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: ((context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                      titleAlignment: ListTileTitleAlignment.center,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      tileColor: const Color.fromARGB(255, 35, 35, 40),
+                      leading: const Icon(Icons.circle_outlined),
+                      title: Text(
+                        'Title',
+                        style: GoogleFonts.inter(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      isThreeLine: true,
+                      subtitle: const Text('when'),
+                      trailing: Image.asset('lib/assets/deleteIcon.png'),
+                    ),
+                  );
+                }),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(45),
+                    child: Image.asset(
+                      'lib/assets/addIcon.png',
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
-          // child: ListView.builder(
-          //   itemCount: 4,
-          //   itemBuilder: ((context, index) => const ListTile(
-          //         leading: Icon(Icons.done, color: Colors.white),
-          //         title: Text('title'),
-          //         trailing: Text('desc'),
-          //       )),
-          // ),
         ),
       ),
     );
